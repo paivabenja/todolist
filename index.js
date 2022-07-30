530fccbc3b0ce3003b39b41cb3b330f79b273d60
paivabenja <paivabenja05@gmail.com>
2022-07-30 15:50:40 -0300

    Adds a row

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const input = document.getElementById("input");
+const createTodo = () => {
+  let row = document.createElement("tr");
+  row.appendChild(
+    document
+      .createElement("td")
+      .appendChild(
+        document
+          .createElement("p")
+          .appendChild(document.createTextNode("example"))
+      )
+  );
+  table.appendChild(row);
+};
+
+const title = document.getElementById("title");
+const titleInput = document.getElementById("tableInput");
 const table = document.getElementById("table");
 const add = document.getElementById("add");
-add.addEventListener("click", createElement());
 
-const createElement = () => {};
+add.addEventListener("click", () => {
+  createTodo();
+});
