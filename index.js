df415112c692e7be7acf6c9bf38a758bbb6d6f77
paivabenja <paivabenja05@gmail.com>
2022-07-30 20:42:33 -0300

    now it adds a todo

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const createTodo = () => {
-  let row = document.createElement("tr");
-  row.appendChild(
-    document
-      .createElement("td")
-      .appendChild(
-        document
-          .createElement("p")
-          .appendChild(document.createTextNode("example"))
-      )
+  let row = table.insertRow();
+  let titulo = row.insertCell();
+  let descripcion = row.insertCell();
+  let remove = row.insertCell();
+  let checkbox = row.insertCell();
+  remove.innerHTML = concat(
+    "<button class='remove' id='",
+    removeIds,
+    "'>remove</button>"
   );
-  table.appendChild(row);
+  checkbox.innerHTML = '<input type="checkbox" />';
+  descripcion.innerHTML = descInput.value;
+  titulo.innerHTML = titleInput.value;
 };
 
-const title = document.getElementById("title");
-const titleInput = document.getElementById("tableInput");
+const titleInput = document.getElementById("titleInput");
+const descInput = document.getElementById("descInput");
 const table = document.getElementById("table");
 const add = document.getElementById("add");
+const remove = document.getElementsByClassName("remove");
+const removeIds = 0;
 
 add.addEventListener("click", () => {
-  createTodo();
+  if (titleInput.value != "" && descInput.value != "") {
+    createTodo();
+    document.getElementById("advise").classList.add("hidden");
+    return;
+  }
+
+  document.getElementById("advise").classList.remove("hidden");
+});
+
+remove.addEventListener("click", () => {
+  return;
 });
